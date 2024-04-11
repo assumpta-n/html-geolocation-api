390c788947189b8db12571d71df6428d1d13b93e
Assumpta <assumptanalubowa@gmail.com>
2024-04-11 20:21:16 +0300

    Creates an https server using a self-signed certificate.

--- geolocation/server.js
+++ geolocation/server.js
@@ -1,15 +1,25 @@
-var http = require('http');
-var fs = require('fs');
+const https = require('https');
+const fs = require('fs');
 
-const port = 8080;
+const httpsPort = 8080;
 const hostName = '127.0.0.1';
 
-http.createServer(function (req, res) {
+const options = {
+  key: fs.readFileSync('ssl/server.key'),
+  cert: fs.readFileSync('ssl/server.crt'),
+  passphrase:'openserver'
+}
+
+const httpsServer = https.createServer(options, (req, res) => {
     fs.readFile('geolocation.html', function(err, data) {
         res.writeHead(200, {'Content-Type': 'text/html'});
         res.write(data);
         return res.end();
     });
-  }).listen(port, hostName, () => {
-    console.log(`Server is running at ${hostName}:${port}`)
-  });
+})
+
+httpsServer.listen(httpsPort, hostName, () => {
+  console.log(`Server is running at ${hostName}:${httpsPort}`)
+});
+
+
