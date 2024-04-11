747fb3ed58a5fae70563febfcd2491d324edf9d6
Assumpta <assumptanalubowa@gmail.com>
2024-04-11 17:02:54 +0300

    Logs the port at which the NodeJS server is running to the terminal

--- geolocation/server.js
+++ geolocation/server.js
@@ -1,10 +1,15 @@
 var http = require('http');
 var fs = require('fs');
 
+const port = 8080;
+const hostName = '127.0.0.1';
+
 http.createServer(function (req, res) {
     fs.readFile('geolocation.html', function(err, data) {
         res.writeHead(200, {'Content-Type': 'text/html'});
         res.write(data);
         return res.end();
     });
-  }).listen(8080);
+  }).listen(port, hostName, () => {
+    console.log(`Server is running at ${hostName}:${port}`)
+  });
